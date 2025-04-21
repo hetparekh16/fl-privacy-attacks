7332e385383b1ee757ece2c4f33e73cb333f102c
Het Parekh <hetparekh16@gmail.com>
2025-04-21 20:21:27 +0200

    Added methods to send and get server updates

--- src/flpa/client.py
+++ src/flpa/client.py
@@ -12,3 +12,12 @@ class FLClient(fl.client.NumPyClient):
         self.test_data = test_data
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
+
+    def get_parameters(self):
+        return [val.cpu().numpy() for val in self.model.state_dict().values()]
+
+    def set_parameters(self, parameters):
+        state_dict = self.model.state_dict()
+        for k, v in zip(state_dict.keys(), parameters):
+            state_dict[k] = torch.tensor(v)
+        self.model.load_state_dict(state_dict)
