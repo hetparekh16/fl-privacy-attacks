53870f7204cca4c9911939e88faf5db5f480b90f
Het Parekh <hetparekh16@gmail.com>
2025-05-05 22:01:29 +0200

    added logging of selected clients

--- src/flpa/server_app.py
+++ src/flpa/server_app.py
@@ -1,20 +1,24 @@
 from flwr.common import Context, ndarrays_to_parameters
 from flwr.server import ServerApp, ServerAppComponents, ServerConfig
-from flwr.server.strategy import FedAvg
 from flpa.task import CNN, get_weights
+from flwr.server.strategy import FedAvg
+
+
+class LoggingFedAvg(FedAvg):
+    def aggregate_fit(self, server_round, results, failures):
+        selected_clients = [client.cid for client, _ in results]
+        print(f"\n🔁 [Round {server_round}] Selected clients: {selected_clients}")
+        return super().aggregate_fit(server_round, results, failures)
 
 
 def server_fn(context: Context):
-    # Read from config
     num_rounds = context.run_config["num-server-rounds"]
     fraction_fit = context.run_config["fraction-fit"]
 
-    # Initialize model parameters
     ndarrays = get_weights(CNN())
     parameters = ndarrays_to_parameters(ndarrays)
 
-    # Define strategy
-    strategy = FedAvg(
+    strategy = LoggingFedAvg(
         fraction_fit=fraction_fit,  # type: ignore
         fraction_evaluate=1.0,
         min_available_clients=2,
@@ -25,5 +29,4 @@ def server_fn(context: Context):
     return ServerAppComponents(strategy=strategy, config=config)
 
 
-# Create ServerApp
 app = ServerApp(server_fn=server_fn)
