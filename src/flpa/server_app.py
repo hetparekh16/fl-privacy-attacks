376e55a7b304fbb3c7e163f860906dd263d20b49
Het Parekh <hetparekh16@gmail.com>
2025-07-06 22:40:36 +0200

    Updated imports and Fed strategy

--- src/flpa/server_app.py
+++ src/flpa/server_app.py
@@ -1,9 +1,10 @@
 from flwr.common import Context, ndarrays_to_parameters
 from flwr.server import ServerApp, ServerAppComponents, ServerConfig
-from flpa.task import CNN, get_weights
+from flpa.task import get_weights
 from flpa.utils import clear_output_directory
 from flpa.fed_strategies.logging_fedavg import LoggingFedAvg
 from flpa.fed_strategies.logging_fedadam import LoggingFedAdam
+from flpa.models import CNN, ResNet
 
 def weighted_average(metrics_list):
     total = sum(num_examples for num_examples, _ in metrics_list)
@@ -27,22 +28,18 @@ def server_fn(context: Context):
     num_rounds = context.run_config["num-server-rounds"]
     fraction_fit = context.run_config["fraction-fit"]
 
-    ndarrays = get_weights(CNN())
+    ndarrays = get_weights(ResNet())
     parameters = ndarrays_to_parameters(ndarrays)
 
-    strategy = LoggingFedAdam(
-        num_rounds=num_rounds, # type: ignore
-        eta=0.01,
-        eta_l=0.1,
-        beta_1=0.9,
-        beta_2=0.999,
-        tau=1e-9,
-        fraction_fit=fraction_fit,
+    strategy = LoggingFedAvg(
+        num_rounds=num_rounds, # type: ignore = 14
+        fraction_fit=fraction_fit,  # type: ignore
         fraction_evaluate=1.0,
         min_available_clients=2,
         initial_parameters=parameters,
-        evaluate_metrics_aggregation_fn=weighted_average,
+        evaluate_metrics_aggregation_fn=weighted_average,  # type: ignore
     )
+
     config = ServerConfig(num_rounds=num_rounds)  # type: ignore
 
     return ServerAppComponents(strategy=strategy, config=config)
